b4e970ce1f1dad24674938b4fc2a78e166b7a171
iowns <iowns@ROYALY>
2024-10-02 08:30:22 +0200

    Add project files.

---
+++ MotivationsFaktorer/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections.Generic;
+string ans;
+string[] arr;
+Console.WriteLine("Indtast på en linje motivationsfaktorer, del med space");
+ans = Console.ReadLine();
+arr = ans.Split(" ");
+Console.WriteLine(CalcAmount(arr));
+static int CalcAmount(string[] array)
+{
+    int amount=0;
+    foreach (var item in array)
+    {
+        ++amount;
+    }
+    return amount;
+}
